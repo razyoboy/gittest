abca7d5df6f8ecc0b92b41796f449b2dcaa745b0
razyoboy <razoreiei@gmail.com>
2021-03-23 22:45:15 +0700

    i dont know

--- graph.py
+++ graph.py
@@ -1,11 +1,11 @@
-import matplotlib.pyplot as mona
 import numpy as np
+import matplotlib.pyplot as mona
 
 def func(x):
-    return (x*np.exp(x)) - (5*np.cos(x*np.pi))
+    return 2*np.exp(-x**2) / np.sqrt(np.pi())
 
 def graph(func):
-    x = np.arange(-5,10,0.0001)
+    x = np.arange(-3,3,0.00001)
     y = func(x)
     mona.plot(x,y)
     mona.show()
