17870e0e1496241364683272b2ec8c80cd7c8e03
razyoboy <78082583+razyoboy@users.noreply.github.com>
2021-03-23 22:47:21 +0700

    updated so it runs

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as mona
 
 def func(x):
-    return 2*np.exp(-x**2) / np.sqrt(np.pi())
+    return 2*np.exp(-x**2) / np.sqrt(np.pi)
 
 def graph(func):
     x = np.arange(-3,3,0.00001)
