e9f15f8bf1491a0ba0e2cd7df35eea83675640c5
razyoboy <razoreiei@gmail.com>
2021-04-02 15:53:12 +0700

    added tensorflow

---
+++ JS/myfirstpornwebsite.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.readFile('porn.html', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data)
+    }
+}).listen(8080);
