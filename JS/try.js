cedd7f17c2001be211a751d876448e88c2e41795
razyoboy <razoreiei@gmail.com>
2021-04-22 14:30:04 +0700

    added quadratic solver

---
+++ JS/try.js
@@ -0,0 +1,9 @@
+console.log('Hi there sister');
+
+var a = prompt("Enter A >");
+var b = prompt('Enter B >');
+var c = prompt('Enter C >');
+var x1, x2;
+
+x1 = (-b - (b**2 - 4*a*c)**0.5)/(2*a);
+x2 = (-b + (B**2 - 4*a*c)**0.5)/(2*a);
