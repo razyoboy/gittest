1ffbc3b6d37541b0ff239a895f701eed1ef1c026
razyoboy <razoreiei@gmail.com>
2021-05-14 17:03:52 +0700

    finally jesus fucking christ

--- FINAL/IntExt/LinearNDDauto.py
+++ FINAL/IntExt/LinearNDDauto.py
@@ -1,27 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([2,4,3.2])
-y = np.array([0.2239,-0.3971])
-'''
+x = np.array([])
+y = np.array([])
+
 print("Please input your x-values (x-axis) [Minimum of Two] \n When done please type 'a'")
 
 while True:
     xint = input('> ')
-    if xint.isnumeric() == True:
-        x = np.append(x, float(xint))
-    elif xint.lower() == 'a':
+    try: 
+        (isinstance(float(xint), float) == True) or (isinstance(float(xint), int) == True)
+        x = np.append(x, float(xint)) 
+    except:
         break
 
 print("Please inout your f(x)-values (y-axis) [NO MORE than Two]\n When done please type 'a'")
 
 while True:
-    yint = input('> ')
-    if yint.isnumeric() == True:
+    yint = input("> ")
+    try:
+        (isinstance(float(yint), float) == True) or (isinstance(float(yint), int) == True)
         y = np.append(y, float(yint))
-    elif yint.lower() == 'a':
+    except:
         break
-'''
+
 def NDD(x,y):
     n = len(x) - 2
     j = 0
@@ -37,18 +39,22 @@ def NDD(x,y):
 ymod = NDD(x,y)
 
 print(f"Interpolated values are (x,y);")
-w = len(y)
+w = len(ymod)
 p = 0
 for k in range (w):
-    print(f"(X{p},Y{p}) -> ({x[0+p]}, {y[0+p]})")
+    print(f"(X{p},Y{p}) -> ({x[0+p]}, {ymod[0+p]})")
     p += 1
 
 plt.scatter(x,ymod)
 plt.plot(x,ymod)
 plt.xlabel('x')
 plt.ylabel('f(x)')
-for x,y in zip(x,ymod):
-    label = "{:.2f}".format(y)
-    plt.annotate(label, (x,y))
+
+for xs,ys in zip(x,ymod):
+    xout = "{:.2f}".format(xs)
+    yout = "{:.2f}".format(ys)
+    label = f"({xout},{yout})"
+    plt.annotate(label, (xs,ys))
+
 plt.title('Linear NDD Interpolation Results')
 plt.show()
